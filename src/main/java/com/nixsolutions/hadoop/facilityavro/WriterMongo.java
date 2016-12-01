bf7c39ca3f849faa1c61f7c9ea65203af19be175
Vyacheslav Biletskiy <biletskiy.vyacheslav@nixsolutions.com>
2016-12-01 16:32:27 +0200

    2 act

--- src/main/java/com/nixsolutions/hadoop/facilityavro/WriterMongo.java
+++ src/main/java/com/nixsolutions/hadoop/facilityavro/WriterMongo.java
@@ -1,8 +1,5 @@
 package com.nixsolutions.hadoop.facilityavro;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.mongodb.*;
 import com.mongodb.util.JSON;
 
@@ -20,24 +17,16 @@ public class WriterMongo {
         try {
             /**** Connect to MongoDB ****/
             MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-            //MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
             // Now connect to your databases
-            DB db = mongoClient.getDB("facilityDb");
+            DB db = mongoClient.getDB("goodJson");
             System.out.println("db.getName() - " + db.getName());
 
-            /**** Get collection / table from 'facilityDb' ****/
+            /**** Get collection / table from 'goodJson' ****/
             // if collection doesn't exists, MongoDB will create it for you
-            DBCollection collection = db.getCollection("facilityCollDb");
+            DBCollection collection = db.getCollection("goodJsonColl");
 
             //JSON parse example
             System.out.println("JSON parse example...");
-
-//            String json = "{'hosting' : 'hostA', 'type' : 'vps', 'clients' : 1000}, { 'hosting' : 'hostB', 'type' : 'dedicated server', 'clients' : 100}, { 'hosting' : 'hostC', 'type' : 'vps', 'clients' : 900}";
-/*
-            String json = "{'database' : 'mkyongDB','table' : 'hosting'," +
-                    "'detail' : {'records' : 99, 'index' : 'vps_index1', 'active' : 'true'}}}";
-
-*/
 
             DBObject dbObject = (DBObject)JSON.parse(jsonData);
             collection.insert(dbObject);
