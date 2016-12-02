32a1371c9fbca03a0ce643ddf47418c5f3c72a7b
Vyacheslav Biletskiy <biletskiy.vyacheslav@nixsolutions.com>
2016-12-02 09:56:10 +0200

    asdasdasd

--- src/main/java/com/nixsolutions/hadoop/facilityavro/Deserializer.java
+++ src/main/java/com/nixsolutions/hadoop/facilityavro/Deserializer.java
@@ -14,8 +14,10 @@ import org.apache.hadoop.fs.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.bson.Document;
 
 class Deserializer {
+/*
     static List<String> getJsonFromAvro(String pathAvroFile) {
         List<String> resultList = new ArrayList<>();
         Configuration config = new Configuration();
@@ -24,7 +26,7 @@ class Deserializer {
         try {
             FileSystem fs = FileSystem.get(config);
             Path path = new Path(pathAvroFile);
-            FSDataInputStream inputStream = fs.open(path);
+         //   FSDataInputStream inputStream = fs.open(path);
             SeekableInput in = new FsInput(path, config);
             DatumReader<GenericRecord> reader
                     = new GenericDatumReader<GenericRecord>();
@@ -39,4 +41,32 @@ class Deserializer {
         }
         return resultList;
     }
+*/
+
+    static List<Document> getJsonFromAvro(String pathAvroFile) {
+        List<Document> resultList = new ArrayList<>();
+        Configuration config = new Configuration();
+        config.addResource(new Path("/HADOOP_HOME/conf/core-site.xml"));
+        config.set("fs.default.name", "hdfs://sandbox.hortonworks.com:8020");
+        try {
+            FileSystem fs = FileSystem.get(config);
+            Path path = new Path(pathAvroFile);
+            //   FSDataInputStream inputStream = fs.open(path);
+            SeekableInput in = new FsInput(path, config);
+            DatumReader<GenericRecord> reader
+                    = new GenericDatumReader<GenericRecord>();
+            FileReader<GenericRecord> fileReader
+                    = DataFileReader.openReader(in, reader);
+            for (GenericRecord datum : fileReader) {
+                String str = datum.toString();
+                Document document = Document.parse(str);
+                resultList.add(document);
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+    }
+
 }
--- src/main/java/com/nixsolutions/hadoop/facilityavro/WriterMongo.java
+++ src/main/java/com/nixsolutions/hadoop/facilityavro/WriterMongo.java
@@ -1,12 +1,16 @@
 package com.nixsolutions.hadoop.facilityavro;
 
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
+import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WriterMongo {
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage : "
                     + "HadoopDFSFileRead <inputfile>");
@@ -15,17 +19,17 @@ public class WriterMongo {
         String inputPath = args[0]+ "/facility.avro";
         List<String> jsonDataList = Deserializer.getJsonFromAvro(inputPath);
         try {
-            /**** Connect to MongoDB ****/
+            *//**** Connect to MongoDB ****//*
             MongoClient mongoClient = new MongoClient(
                     new MongoClientURI("mongodb://localhost:27017"));
-            /**** Now connect to databases ****/
+            *//**** Now connect to databases ****//*
             DB db = mongoClient.getDB("facilityDb");
-            /**** Get collection / table from 'goodJson' ****/
+            *//**** Get collection / table from 'goodJson' ****//*
             DBCollection collection = db.getCollection("facilityDbColl");
           //  DBCursor cursorDocJSON = collection.find();
-            /**** Remove old data ****/
+            *//**** Remove old data ****//*
             collection.remove(new BasicDBObject());
-            /**** Insert new data ****/
+            *//**** Insert new data ****//*
             for (String entity: jsonDataList) {
                 DBObject dbObject = (DBObject)JSON.parse(entity);
                 collection.insert(dbObject);
@@ -33,5 +37,40 @@ public class WriterMongo {
             } catch (MongoException e) {
             e.printStackTrace();
         }
+    }*/
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage : "
+                    + "HadoopDFSFileRead <inputfile>");
+            System.exit(1);
+        }
+        String inputPath = args[0]+ "/facility.avro";
+        List<Document> jsonDataList = Deserializer.getJsonFromAvro(inputPath);
+        try {
+            MongoClient mongoClient = new MongoClient(
+                    new MongoClientURI("mongodb://localhost:27017"));
+            MongoDatabase db = mongoClient.getDatabase("mongoClient");
+//            DB db = mongoClient.getDB("facilityDb");
+
+            MongoCollection<Document> collection = db.getCollection("mongoClientColl");
+//            DBCollection collection = db.getCollection("facilityDbColl");
+            //  DBCursor cursorDocJSON = collection.find();
+
+
+            collection.drop();
+//            collection.remove(new BasicDBObject());
+
+
+            collection.insertMany(jsonDataList);
+
+/*            for (String entity: jsonDataList) {
+                DBObject dbObject = (DBObject)JSON.parse(entity);
+                collection.insert(dbObject);
+            }*/
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
     }
+
 }
