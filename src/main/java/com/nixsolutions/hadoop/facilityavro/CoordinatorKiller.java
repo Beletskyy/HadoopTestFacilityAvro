df80b8ac386c5778df1cf144ab32a59cc223b7b7
Vyacheslav Biletskiy <biletskiy.vyacheslav@nixsolutions.com>
2016-12-02 17:02:49 +0200

    refactoring

--- src/main/java/com/nixsolutions/hadoop/facilityavro/CoordinatorKiller.java
+++ src/main/java/com/nixsolutions/hadoop/facilityavro/CoordinatorKiller.java
@@ -11,29 +11,32 @@ import java.io.StringReader;
 import java.util.List;
 import java.util.Properties;
 
-/**
- *
- */
+
 public class CoordinatorKiller extends PropertyHolder {
-    public void killCoord(String coordinatorName) throws IOException {
-        fsin = fs.open(propertyFile);
+
+    public void killCoordinator(String coordinatorName) {
         ByteArrayOutputStream bo = new ByteArrayOutputStream();
         byte[] b = new byte[1];
-        while(fsin.read(b)!=-1){
-            bo.write(b);
+        try {
+            fsin = fs.open(propertyFile);
+            while(fsin.read(b)!=-1){
+                bo.write(b);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         String properties = new String(bo.toByteArray());
         //load properties for getting host IP
         Properties prop = new Properties();
-        prop.load(new StringReader(properties));
-        String host = prop.getProperty("host");
-
-        //killing coordinator
-        OozieClient oozie = new AuthOozieClient("http://" + host + ":11000/oozie");
-        List<CoordinatorJob> joblist = null;
         try {
-            joblist = oozie.getCoordJobsInfo("status=RUNNING", 1, 100);
-//    		System.setProperty("user.name", "root");
+            prop.load(new StringReader(properties));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String host = prop.getProperty("host");
+        OozieClient oozie = new AuthOozieClient("http://" + host + ":11000/oozie");
+        try {
+            List<CoordinatorJob> joblist = oozie.getCoordJobsInfo("status=RUNNING", 1, 100);
             for (int i = 0; i < joblist.size(); i++) {
                 String result = joblist.get(i).getAppName();
                 if (i > 0 && result.equals(coordinatorName)) {
